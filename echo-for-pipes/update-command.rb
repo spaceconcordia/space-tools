c94462f8f8f1d2cef04d40986b4428d2637ef0d3
spaceconcordia <spaceconcordia@Charizard.(none)>
2013-12-20 19:29:22 -0500

    Fixed update command to split into pieces

--- echo-for-pipes/update-command.rb
+++ echo-for-pipes/update-command.rb
@@ -1,19 +1,35 @@
-q6_path  = ARGV[0] || ""
+q6_path  = ARGV[0] || "/home/apps/new/a"
 filename = ARGV[1] || "output"
 
 file = File.new(filename, "r")
 
+q6_path_length_bytes = q6_path.length.to_s.each_byte.map do |b|
+    b.to_s(16)
+end
 
-q6_path_hex     = q6_path.each_byte.map {|b| "\\\\x#{b.to_s(16)}" }.join
-q6_path_len_hex = q6_path.length.to_i.to_s.each_byte.map {|b| "\\\\x#{b.to_s(16)}" }.join
-data_hex        = file.each_byte.map {|b| "\\\\x#{b.to_s(16)}" }.join
-data_len_hex    = "192".each_byte.map {|b| "\\\\x#{b.to_s(16)}" }.join
+(3 - q6_path_length_bytes.length).times {|i| q6_path_length_bytes.unshift("30")}
 
-command_length     = 3 + q6_path.length + 3 + "f0VMRgEBAUhpIFdvcmxkCgI=".length
-command_length_hex = command_length.to_s(16)
-
-puts "echo -n -e \\\\x#{command_length_hex} > Inet-w-com-r\n"
-puts "echo -n -e \\\\x32#{q6_path_len_hex}#{q6_path_hex}#{data_len_hex}#{data_hex} > Dnet-w-com-r\n"
-puts "echo -n -e \\\\xFF > Inet-w-com-r\n"
+q6_path_bytes = q6_path.each_byte.map {|b| b.to_s(16)} 
 
 
+FRAME_LENGTH = 190
+data_length = FRAME_LENGTH - q6_path_length_bytes.length - q6_path_bytes.length
+
+data_bytes    = file.each_byte.map {|b| b.to_s(16) }
+loop_times  = (data_bytes.length / data_length.to_f).ceil
+
+q6_path_hex = "#{q6_path_length_bytes.map{|s| "\\\\x#{s}"}.join}" + "#{q6_path_bytes.map{|s| "\\\\x#{s}"}.join}"
+
+
+loop_times.times do |i|       
+    start  = i * data_length
+    finish = i + 1 * data_length
+    if finish > data_bytes.length
+        finish = data_bytes.length 
+    end
+
+    data_bytes_hex = data_bytes[start..finish].map{|s| "\\\\x#{s}"}.join
+
+    puts "echo -n -e \\\\x32#{q6_path_hex}#{data_bytes_hex} > Dnet-w-com-r\n"
+    puts "echo -n -e \\\\x21 > Dnet-w-com-r\n"
+end
