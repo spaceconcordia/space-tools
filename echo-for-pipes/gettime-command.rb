adf566d930b8d337c4dae78203617c9e4a46b7e1
spaceconcordia <spaceconcordia@mustang.(none)>
2013-12-22 16:06:51 -0500

    Added gettime and reboot commands

---
+++ echo-for-pipes/gettime-command.rb
@@ -0,0 +1,3 @@
+puts "echo -n -e \\\\x01 > Inet-w-com-r\n"
+puts "echo -n -e \\\\x31 > Dnet-w-com-r\n"
+puts "echo -n -e \\\\xFF > Inet-w-com-r\n"
---
+++ echo-for-pipes/reboot-command.rb
@@ -0,0 +1,3 @@
+puts "echo -n -e \\\\x01 > Inet-w-com-r\n"
+puts "echo -n -e \\\\x34 > Dnet-w-com-r\n"
+puts "echo -n -e \\\\xFF > Inet-w-com-r\n"
